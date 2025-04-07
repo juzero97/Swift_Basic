d84af3c574af93f147c0ee0d5e92a7a67d7033f3
juzero97 <juzeroo97@gmail.com>
2025-04-07 21:07:34 +0900

    SwiftUi로 배우는 Swift문법-반복문

---
+++ SwiftUI_Swift/swsf/swsf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swsf
+//
+//  Created by 송주영 on 4/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Swift/swsf/swsf/swsfApp.swift
@@ -0,0 +1,17 @@
+//
+//  swsfApp.swift
+//  swsf
+//
+//  Created by 송주영 on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct swsfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
